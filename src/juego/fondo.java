ad786b1bb04766b59082ad325c6fe9f26a8570c7
gabri <gabri@DESKTOP-ERKQVO2>
2022-03-18 22:46:33 +0100

    retoque de comentario

--- src/juego/fondo.java
+++ src/juego/fondo.java
@@ -4,25 +4,52 @@ import java.awt.Graphics2D;
 
 import javax.swing.ImageIcon;
 
+/**
+ * 
+ * Esta clase es el fondo del juego 
+ * 
+ * @author Gabriel Vargas y Hodei Axpe
+ * 
+ * @apiNote Son dos fondo porque se va a ir moviendo entonces para que no suceda un error en la textura del fondo
+ *
+ */
 public class fondo {
+	
+	// ************************************************ Variables *************************************************************** //
 
+
+	// variables de objetos de otras clases //
 	juego jueguito;
+	
+	// variables de clases de java //
 	ImageIcon fondo = new ImageIcon();
 
+	// posicion por defecto //
 	int x1 = 0;
 	int x2 = 0;
-
 	int y1 = 1300;
 	int y2 = 0;
 
+	// variable del tamaño del fondo // 
 	int anchoFondo = 700;
 	int alto_fondo = 1300;
 
+	// ************************************************ Constructor ************************************************************ //
+	
 	public fondo(juego juego) {
 
 		this.jueguito = juego;
 	}
+	
+	// ************************************************ Metodos *************************************************************** //
 
+	/**
+	 * 
+	 *Este metodo hace que mueve el fondo
+	 *
+	 *@apiNote Cuando llega a un cierto limite resetea las posiciones para que el fondo no se destruya en el movimiento
+	 *
+	 */
 	public void mover() {
 
 		y1 -= 4;
@@ -35,14 +62,22 @@ public class fondo {
 		}
 	}
 
+	/**
+	 * Construye la imagen de el fondo
+	 */
 	public void inicializar() {
 
 		fondo = new ImageIcon("multimedia/fondo1.jpg");
 	}
-
-	// --------- paint ---------//
-
-	// dibuja los graficos2d
+	
+	/**
+	 * 
+	 * Pinta la imagen en el plano
+	 * 
+	 * @param g = pintar
+	 * 
+	 * @apiNote pinta dos porque son dos fondos
+	 */
 	public void paint(Graphics2D g) {
 
 		g.drawImage(fondo.getImage(), x1, y1, anchoFondo, alto_fondo, null);
